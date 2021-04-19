414d582935e9cc88c0f42cd0e871a75577acac80
Jonathan Narwold <jonathan.narwold@beam.dental>
2021-04-19 08:04:38 -0400

    add codeowners action

---
+++ actions/codeowners/index.js
@@ -0,0 +1,21 @@
+const github = require('@actions/github')
+const core = require('@actions/core')
+
+const { context } = github
+const { owner, repo } = context.repo
+
+const githubToken = core.getInput('githubToken')
+const octokit = github.getOctokit(githubToken)
+
+const run = async () => {
+  const labels = await octokit.issues.listLabelsOnIssue({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  })
+  console.log(labels)
+  console.log(context.payload.requested_team.name)
+  console.log(context)
+}
+
+run()
